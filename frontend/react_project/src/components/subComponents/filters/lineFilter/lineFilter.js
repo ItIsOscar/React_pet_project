2f89fb42c89ef27aa71e4ed3ebda8af839aa594a
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-04-17 18:16:09 +0500

    Рефакторинг анимации для линейных фильтров

--- frontend/react_project/src/components/subComponents/filters/lineFilter/lineFilter.js
+++ frontend/react_project/src/components/subComponents/filters/lineFilter/lineFilter.js
@@ -1,9 +1,9 @@
-import { useState } from "react"
+import { useRef, useState, forwardRef } from "react"
 import "./lineFilter.scss"
 
-function CheckBox({children, value, name, onChange, ischecked = false}) {
+const CheckBox = forwardRef(({children, value, name, onChange, ischecked = false}, ref) => {
   return (
-    <label className="checkBox">
+    <label className="checkBox" ref={ref}>
       {ischecked ? 
         <input type="radio" onClick={onChange} name={name} value={value} id={value} defaultChecked={ischecked}/>  : 
         <input type="radio" onClick={onChange} name={name} value={value} id={value} />
@@ -11,68 +11,77 @@ function CheckBox({children, value, name, onChange, ischecked = false}) {
       <span>{children}</span>
     </label>
   )
-}
+})
 
 export default function LineFilter() {
-  let [savePosition, setCurrentPosition] = useState(0)
-  let [savePlace, setCurrentPlace] = useState(0)
-  function takePlace(e) {
-    // console.log(savePosition)
-    let allCheckBox = Array.from(document.querySelectorAll("input[name=typeOfSallers]"))
-    let line = document.querySelector(".line")    
-    let boxsWidth = []
-    for(let inx of allCheckBox) {
-      boxsWidth.push(inx.labels[0].offsetWidth)
-    }
-    
-    let indexTo = allCheckBox.findIndex((el) => el.checked)
-    let currentPosition = savePosition
-    if(savePlace == indexTo) {
-      console.log("nothin changing")
-    } else if(savePlace > indexTo) {
-      setCurrentPlace(indexTo)
-      currentPosition = savePosition - boxsWidth[savePlace - 1] 
-        if(savePlace - 2 == indexTo) {
-          currentPosition = currentPosition - boxsWidth[savePlace - 2]
-        }
+  let [currentLineMargin, setCurrentLineMargin] = useState(0)
+  let [currentTabInx, setCurrentTabInx] = useState(0)
+  const firstLabel = useRef(null)
+  const secondLabel = useRef(null)
+  const thirdLabel = useRef(null)
+  let line = useRef(null)
+
+  function findActiveIndex(allLabes) {
+    return allLabes.findIndex((el) => el.el.children[0].checked) //active == to
+  }
+
+  function setLineMargin(activeIdx, allLabes) {
+    let newLineMargin = currentLineMargin
+    if(currentTabInx < activeIdx) {
+      for(let current = currentTabInx; current < activeIdx; current++) {
+        newLineMargin = newLineMargin + allLabes[current].elWidth
+      }
     } else {
-      setCurrentPlace(indexTo)
-      currentPosition = savePosition + boxsWidth[savePlace]
-      // console.log("1 step", currentPosition)
-        if(savePlace + 2 == indexTo) {
-          // console.log("2 step", boxsWidth[savePlace + 1])
-          currentPosition = currentPosition + boxsWidth[savePlace + 1]
-        }
+      for(let current = currentTabInx; current > activeIdx; current--) {
+        newLineMargin = newLineMargin - allLabes[current - 1].elWidth
+      }
     }
-    setCurrentPosition(currentPosition)
-    line.style.transform = `translate(${currentPosition}px, 0px)`
+    line.current.style.transform = `translate(${newLineMargin}px, 0px)`
+    setCurrentLineMargin(newLineMargin)
+  }
 
-    let toSpan = document.querySelector("input[name=typeOfSallers]:checked + span")
-    line.style.width = `${toSpan.offsetWidth}px`
+  function setLineWidth(activeIdx, allLabes) {
+    let spanOnActiveInputBox = allLabes[activeIdx].el.children[1]
+    line.current.style.width = `${spanOnActiveInputBox.offsetWidth}px`
+  }
 
-    // console.log("event", e)
-    // console.log("all",allCheckBox)
-    // console.log(boxsWidth)
-    // console.log("savePosition", currentPosition)
-    // console.log("savePlace", savePlace)
-    // console.log("toInx", indexTo)
-    // console.log("to", toSpan)
+  function setKeyFrameForLine() {
+    let allLabes = [
+      {
+        el: firstLabel.current,
+        elWidth: firstLabel.current.offsetWidth
+      }, 
+      {
+        el: secondLabel.current,
+        elWidth: secondLabel.current.offsetWidth
+      }, 
+      {
+        el: thirdLabel.current,
+        elWidth: thirdLabel.current.offsetWidth
+      }, 
+    ]
+
+    let activeIdx = findActiveIndex(allLabes)
+    setLineMargin(activeIdx, allLabes)
+    setLineWidth(activeIdx, allLabes)
+    setCurrentTabInx(activeIdx)
   } 
+
   return (
     <div className="mainFilters" >
       <div>
         <div className="filters">
-          <CheckBox ischecked={true} name="typeOfSallers" value={"all"} onChange={takePlace}>
+          <CheckBox ischecked={true} ref={firstLabel} name="typeOfSallers" value={"all"} onChange={setKeyFrameForLine}>
             Все товары
           </CheckBox>
-          <CheckBox name="typeOfSallers" value={"business"} onChange={takePlace}>
+          <CheckBox name="typeOfSallers" ref={secondLabel} value={"business"} onChange={setKeyFrameForLine}>
             Бизнес
           </CheckBox>
-          <CheckBox name="typeOfSallers" value={"sellers"} onChange={takePlace}>
+          <CheckBox name="typeOfSallers" ref={thirdLabel} value={"sellers"} onChange={setKeyFrameForLine}>
             Частники
           </CheckBox>
         </div>
-        <span className="line"></span>
+        <span className="line" ref={line}></span>
       </div>
       <div className="filtersOptions">
         <button type="submit">Подвердить фильтры</button>
