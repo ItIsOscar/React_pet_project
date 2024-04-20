716372d815402659a08ef350cad43c0f0d38a561
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-04-20 20:30:08 +0500

    Добавление подвала для навигации(временно; недоделано)

--- frontend/react_project/src/components/routerConnector/navigation/navigation.js
+++ frontend/react_project/src/components/routerConnector/navigation/navigation.js
@@ -12,7 +12,7 @@ import basket_border from "../../../assets/basket_border.svg"
 import basket_full from "../../../assets/basket_full.svg"
 import { useRef, useState } from "react"
 
-function TopButtons({ href, hoverSrc, src, title, handleClick = null}) {
+function SiteButtons({ href, hoverSrc, src, title, handleClick = null}) {
   let [isHover, setHover] = useState(false)
   
   return (
@@ -26,10 +26,20 @@ function TopButtons({ href, hoverSrc, src, title, handleClick = null}) {
   )
 }
 
+function CatalogButtons({ children, pic }) {
+  return (
+    <li>
+      <span>{children}</span>
+      <img src={pic} />
+    </li>
+  )
+}
+
 export default function Navigation({toggleIsReg}) {
   let [status, setStatus] = useState("")
   let lastScroll = useRef(0)
   const nav = useRef(null)
+  const catalogNav = useRef(null)
 
   function handleSetStatus() {
     let currentScroll = window.scrollY
@@ -48,8 +58,10 @@ export default function Navigation({toggleIsReg}) {
     switch(status) {
       case "down":
         nav.current.style.transform = "translate(0, -100%)"
+        catalogNav.current.style.maxHeight = "0px"
         break
       case "top":
+        catalogNav.current.style.maxHeight = "100%"
       case "up":
         nav.current.style.transform = "translate(0, 0)"
         break
@@ -63,22 +75,29 @@ export default function Navigation({toggleIsReg}) {
 
   return (
     <div className="navigation" ref={nav}>
-      <a href="/">
-        <img src={logo}/>
-      </a>
-      <div className="navContent">
-        <select className="language">
-          <option>Русский</option>
-          <option>English</option>
-          <option>O`zbek</option>
-        </select>
-        <div className="navOption">
-          <TopButtons href="/favourite" title="Избранное" src={heart_border} hoverSrc={heart_full} />
-          <TopButtons handleClick={toggleIsReg} title="Войти" src={user_border} hoverSrc={user_full} />
-          <TopButtons href="/basket" title="Корзина" src={basket_border} hoverSrc={basket_full} />
-          <TopButtons href="/delivery" title="Доставка" src={delivery_border} hoverSrc={delivery_full} />
+      <div className="siteNav">
+        <a href="/">
+          <img src={logo}/>
+        </a>
+        <div className="navContent">
+          <select className="language">
+            <option>Русский</option>
+            <option>English</option>
+            <option>O`zbek</option>
+          </select>
+          <div className="navOption">
+            <SiteButtons href="/favourite" title="Избранное" src={heart_border} hoverSrc={heart_full} />
+            <SiteButtons handleClick={toggleIsReg} title="Войти" src={user_border} hoverSrc={user_full} />
+            <SiteButtons href="/basket" title="Корзина" src={basket_border} hoverSrc={basket_full} />
+            <SiteButtons href="/delivery" title="Доставка" src={delivery_border} hoverSrc={delivery_full} />
+          </div>
         </div>
       </div>
+      <ul className="catalogNav" ref={catalogNav}>
+        <CatalogButtons pic=""> Электоника</CatalogButtons>
+        <CatalogButtons pic="">Машины</CatalogButtons>
+        <CatalogButtons pic="">Игрушки</CatalogButtons>
+      </ul>
     </div>
   )
 }
