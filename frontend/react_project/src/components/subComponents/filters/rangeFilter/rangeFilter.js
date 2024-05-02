b0d8a33b007b33a35b5c35b35050871691681a77
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-05-02 19:47:09 +0500

    Фильтры

--- frontend/react_project/src/components/subComponents/filters/rangeFilter/rangeFilter.js
+++ frontend/react_project/src/components/subComponents/filters/rangeFilter/rangeFilter.js
@@ -1,19 +1,44 @@
+import { useRef } from "react"
 import "./rangeFilter.scss"
 
-export default function RangeFilter({title, name}) {
+export default function RangeFilter({title, name, value}) {
+  let inputFrom = useRef(null)
+  let inputTo = useRef(null)
+  let fromValue 
+  if(sessionStorage.getItem(`${name}From`)) {
+    fromValue = sessionStorage.getItem(`${name}From`)
+  } else {
+    sessionStorage.setItem(`${name}From`, '0')
+  }
+
+  let toValue 
+  if(sessionStorage.getItem(`${name}To`)) {
+    toValue = sessionStorage.getItem(`${name}To`)
+  } else {
+    sessionStorage.setItem(`${name}To`, '0')
+  }
+
+  function handleChangeFrom() {
+    sessionStorage[inputFrom.current.name] = inputFrom.current.value
+  }
+
+  function handleChangeTo() {
+    sessionStorage[inputTo.current.name] = inputTo.current.value
+  }
+
   return (
     <div className="numberFilter">
         <h3>{title+":"}</h3>
-        <form name="name">
+        <div>
           <label>
             <span>От:</span>
-            <input type="number" name="from"></input>
+            <input type="number" name={`${name}From`} placeholder="1000" onChange={handleChangeFrom} ref={inputFrom} defaultValue={fromValue}/>
           </label>
           <label>
             <span>До:</span>
-            <input type="number" name="to"></input>
+            <input type="number" name={`${name}To`} placeholder="100.0000.000" onChange={handleChangeTo} ref={inputTo} defaultValue={toValue} />
           </label>
-        </form>
+        </div>
     </div>
   )
 }
--- frontend/react_project/src/components/subComponents/filters/selectorFilter/selectorFilter.js
+++ frontend/react_project/src/components/subComponents/filters/selectorFilter/selectorFilter.js
@@ -1,13 +1,28 @@
+import { useRef } from "react"
 import "./selectorFilter.scss"
 
 export default function SelectorFilter({title, name, values}) {
+  let selector = useRef(null)
   let options = values.map((el) => {
-    return <option value={el} key={el}>{el}</option>
+    return  <option value={el} key={el}>{el}</option>
   })
+
+  let active 
+  if(sessionStorage.getItem(name)) {
+    active = sessionStorage.getItem(name)
+  } else {
+    sessionStorage.setItem(name, "all")
+    active = sessionStorage.getItem(name)
+  }
+
+  function handleChange() {
+    sessionStorage[name] = selector.current.value
+  }
+
   return (
     <div className="selectorFilter">
       <h3>{title + ":"}</h3>
-      <select name={name}>
+      <select name={name} defaultValue={active} onChange={handleChange} ref={selector}>
         {options}
       </select>
     </div>
