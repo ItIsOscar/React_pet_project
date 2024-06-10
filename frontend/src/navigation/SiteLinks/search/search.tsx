fba87cb4bf2c60e914a8e1a2c9ca94c18b3a4104
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-06-10 10:13:38 +0500

    Добавление сброса значение поиска, изменение компонента

--- frontend/src/navigation/SiteLinks/search/search.tsx
+++ frontend/src/navigation/SiteLinks/search/search.tsx
@@ -1,7 +1,7 @@
-import { strict } from "assert"
 import "./search.scss"
 import {FormEvent, RefObject, useRef} from "react"
 import { SearchType as FormTypeModel } from "./search.interface"
+const CROSS_ICON = "https://ikonki.svgpng.ru/wp-content/uploads/2021/12/Krestiksvgpng.ru_.png"
 
 export default function Search() {
   const FORM_TYPE: RefObject<HTMLSelectElement> = useRef(null)
@@ -11,15 +11,14 @@ export default function Search() {
   let typesJSX: JSX.Element[] = listType.map(model => (
     <option value={model.value}>{model.title}</option>
   ))
-  
+
   function fetchType() {
     fetch(`search/${FORM_TYPE.current!.value}/all`, {
       method: "GET",
     })
   }
 
-  function fetchNameWithType(event: FormEvent) {
-    event.preventDefault()  
+  function fetchNameWithType() {
     type validateFetchString = string
     const formValidateName: validateFetchString = 
       FORM_NAME.current!.value.trim().split(" ").filter(word => word != "").join(" ")
@@ -28,15 +27,31 @@ export default function Search() {
       method: "GET",
     })
   }
+
+  function onResetFormName() {
+    FORM_NAME.current!.value = ""
+    fetchNameWithType()
+  }
+  function onSubmitForm(event: FormEvent) {
+    event.preventDefault()
+    fetchNameWithType()
+  }
+  function onChangeFormType(): void {
+    fetchType()
+  }
+
   return (
-    <form className="searchBar" onSubmit={fetchNameWithType}>
-      <select name="type" onChange={fetchType} ref={FORM_TYPE}>
+    <form className="searchBar">
+      <select name="type" onChange={onChangeFormType} ref={FORM_TYPE}>
         {typesJSX}
       </select>
-      <div className="searchInput">
+      <div className="inputName">
         <input name="name" type="text" ref={FORM_NAME} placeholder="Поиск"></input>
       </div>
-      <button type="submit">Поиск</button>
+      <button type="button" className="resetTypeButton" onClick={onResetFormName}>
+        <img src={CROSS_ICON} />
+      </button>
+      <button className="submitButton" onClick={event => onSubmitForm(event)}>Поиск</button>
     </form>
       
   )
