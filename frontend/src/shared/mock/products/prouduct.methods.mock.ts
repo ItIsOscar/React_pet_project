725b928aa8a01d810ead7dff30cf3fe90fdd909a
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-06-22 20:07:36 +0500

    Исправление языка для моков

--- frontend/src/shared/mock/products/prouduct.methods.mock.ts
+++ frontend/src/shared/mock/products/prouduct.methods.mock.ts
@@ -1,12 +1,10 @@
-import userMock from "../users/user.methods.mock"
-import USERS_LIST from "../users/users.list.mock"
+
 import PRODUCT_LIST, { PRODUCT } from "./product.list.mock"
 
 class ProductsMethods {
   products = PRODUCT_LIST
   
   findProductsById = (id: number | string): PRODUCT => {
-    // let numberId: number = 
     return this.products.find((el) => el.id == id)! //Возможен костыль
   }
 }
