b59f05019578d0b8679ee4d24482f963cbee46a1
ItIsOscar <153256994+ItIsOscar@users.noreply.github.com>
2024-06-22 20:06:05 +0500

    router to tsx

--- frontend/src/shared/mock/products/prouduct.methods.mock.tsx
+++ frontend/src/shared/mock/products/prouduct.methods.mock.tsx
@@ -1,12 +1,13 @@
 import userMock from "../users/user.methods.mock"
 import USERS_LIST from "../users/users.list.mock"
-import PRODUCT_LIST from "./product.list.mock"
+import PRODUCT_LIST, { PRODUCT } from "./product.list.mock"
 
 class ProductsMethods {
   products = PRODUCT_LIST
   
-  findProductsById = (id: number) => {
-    return this.products.find((el) => el.id == id)
+  findProductsById = (id: number | string): PRODUCT => {
+    // let numberId: number = 
+    return this.products.find((el) => el.id == id)! //Возможен костыль
   }
 }
 let productsMock = new ProductsMethods
